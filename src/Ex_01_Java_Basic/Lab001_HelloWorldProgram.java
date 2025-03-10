7faa25966c330a2110126e931e6b2e38ee1a59a0
supriyadarade <supriya.darade94@gmail.com>
2025-03-10 18:47:13 +0530

    6th March

---
+++ src/Ex_01_Java_Basic/Lab001_HelloWorldProgram.java
@@ -0,0 +1,8 @@
+package Ex_01_Java_Basic;
+
+public class Lab001_HelloWorldProgram {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        // Write a program of factorial
+    }
+}
