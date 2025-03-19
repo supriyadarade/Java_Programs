0d022d524006259043653b234b5d0408f4af7677
supriyadarade <supriya.darade94@gmail.com>
2025-03-19 12:31:25 +0530

    12th March

---
+++ src/Tasks/Task_002_Equation_12_March.java
@@ -0,0 +1,20 @@
+package Tasks;
+
+public class Task_002_Equation_12_March {
+
+    public static void main(String[] args) {
+
+        double x = 10;
+        double y = 10;
+        double z = 10;
+
+        double result = Math.cbrt(Math.pow(x, 2) + Math.pow(y, 2) - Math.abs(z));
+
+        System.out.println("Result: " + result);
+    }
+
+
+
+
+
+}
