ad57c85e3e2edede406c2af4546683dc7538bff3
supriyadarade <supriya.darade94@gmail.com>
2025-04-14 14:30:42 +0530

    27th March

--- src/Ex_16_Arrays/Program_158_CLI.java
+++ src/Ex_16_Arrays/Program_158_CLI.java
@@ -2,7 +2,7 @@ package Ex_16_Arrays;
 
 public class Program_158_CLI {
     public static void main(String[] args) {
-        for (int i = 0; i < args.length; i++) {
+        for (int i = 0; i < args.length; i++) { // you can also add the strings in the user input
             System.out.println(args[i]);
         }
     }
