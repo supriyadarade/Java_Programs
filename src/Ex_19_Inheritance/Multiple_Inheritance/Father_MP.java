360bdc2f9ecd71c41fd0148b8b203554b4b0680b
supriyadarade <supriya.darade94@gmail.com>
2025-04-16 18:25:06 +0530

    2nd April

--- src/Ex_19_Inheritance/Multiple_Inheritance/Father_MP.java
+++ src/Ex_19_Inheritance/Multiple_Inheritance/Father_MP.java
@@ -2,6 +2,7 @@ package Ex_19_Inheritance.Multiple_Inheritance;
 
 public class Father_MP {
     void money(){
-        System.out.println("1 Cr");
+        System.out.println("1 Cr"); //Multiple inheritance is not possible
+        // it is possible when we can use the abstraction and interfaces
     }
 }
